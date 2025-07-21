480ed0ac12cf66289d555a32cfb04e3b6719d95d
Steitz <shteizel@mail.ru>
2025-07-21 22:27:25 +0300

    Update task3.java

--- task3.java
+++ task3.java
@@ -1,1 +1,16 @@
-
+import java.util.Scanner;
+public class Main {
+    public static double calculateCustoms (int vPrice, int vWeight){
+        double res = vPrice*0.01+vWeight*100;
+        return res;
+    }
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print ( "Введите цену товара (в руб.):");
+        int price = scanner.nextInt();
+        System.out.print ( "Введите вес товара (в кг):");
+        int weight = scanner.nextInt();
+        double tax = calculateCustoms(price,weight);
+        System.out.println ( "Размер пошлины (в руб.) составит: "+tax);
+    }
+}
