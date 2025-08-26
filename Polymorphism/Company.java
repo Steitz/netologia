e07d63fd8af754c58f8c6d8ac635a41f02b9cadf
Steitz <shteizel@mail.ru>
2025-08-26 17:47:33 +0300

    Update Company.java

--- Polymorphism/Company.java
+++ Polymorphism/Company.java
@@ -2,15 +2,14 @@ import deals.Deal;
 import taxes.TaxSystem;
 
 public class Company {
-    public char title; // (название)
-    public int debit = 0;  // (доходы)
-    public int credit = 0; // (расходы)
-    public TaxSystem tax;
-    public Deal deal = new Deal("хлеб", 0, 100);
+    protected char title; // (название)
+    protected int debit = 0;  // (доходы)
+    protected int credit = 0; // (расходы)
+    protected TaxSystem USN;
 
-    Company(char title, TaxSystem tax) {
+    Company(char title, TaxSystem USN) {
         this.title = title;
-        this.tax = tax;
+        this.USN = USN;
     }
 
     public void shiftMoney(int amount) {
@@ -21,15 +20,14 @@ public class Company {
     }
 
     public void setTaxes(TaxSystem companyTax) {
-        this.tax = companyTax;
+        this.USN = companyTax;
     }
 
     //рассчитает с помощью выбранной этой компанией системы налогооблажения размер налогов;
     //выведет сообщение на экран вида: Компания <название> уплатила налог в размере: <сумма> руб.;
     //обнулит счётчики debit и credit.
     public void payTaxes(TaxSystem companyTax) {
-        int sumTax;
-        sumTax = companyTax.calcTaxFor(debit, credit);
+        int sumTax = companyTax.calcTaxFor(debit, credit);
         System.out.printf("Компания %s уплатила налог в размере: %d руб.\n", this.title, sumTax);
         credit = 0;
         debit = 0;
@@ -44,14 +42,17 @@ public class Company {
         boolean flag = false;
         System.out.println("Сделки:");
         for (int i = 0; i < deals.length; i++) {
-            this.credit += deals[i].creditChange;
-            this.debit += deals[i].debitChange;
-            if (deals[i].creditChange == 0) flag = false;
-            else flag = true;
-            System.out.println(deals[i].comment + (flag ? " расход = " + deals[i].creditChange : " доход  = " + deals[i].debitChange));
+            this.credit += deals[i].getCreditChange();
+            this.debit += deals[i].getDebitChange();
+            if (deals[i].getCreditChange() == 0) {
+                flag = false;
+            } else {
+                flag = true;
+            }
+            System.out.println(deals[i].getComment() + (flag ? " расход = " + deals[i].getCreditChange() : " доход  = " + deals[i].getDebitChange()));
         }
         System.out.println("Итого: доход = " + this.debit + " расход = " + this.credit + "\n");
-        payTaxes(tax);
+        payTaxes(USN);
         return (this.debit - this.credit);
 
     }
