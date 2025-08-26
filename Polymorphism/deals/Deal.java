9844a75ad180f34baca26eb7d0bb280c3c0b7048
Steitz <shteizel@mail.ru>
2025-08-26 17:48:15 +0300

    Update Deal.java

--- Polymorphism/deals/Deal.java
+++ Polymorphism/deals/Deal.java
@@ -1,13 +1,13 @@
-package deals;
+ackage deals;
 
 //Создайте в вашем проекте дополнительный класс Deal, объект которого будет описывать сделку,
 // что может провернуть компания. Каждая сделка сопровождается комментарием (comment),
 // суммой расходов (creditChange) и суммой доходов (debitChange).
 // Создайте конструктор, который принимает эти три параметра.
 public class Deal {
-    public String comment;
-    public int creditChange;
-    public int debitChange;
+    protected String comment;
+    protected int creditChange;
+    protected int debitChange;
 
     public Deal(String comment, int creditChange, int debitChange) {
         this.comment = comment;
@@ -15,4 +15,20 @@ public class Deal {
         this.debitChange = debitChange;
 
     }
+    public String getComment() {
+        return comment;
+    }
+    public void setCreditChange(int creditChange) {
+        this.creditChange = creditChange;
+    }
+    public int getCreditChange() {
+        return creditChange;
+    }
+    public int getDebitChange() {
+        return debitChange;
+    }
+    public void setDebitChange(int debitChange) {
+        this.debitChange = debitChange;
+    }
 }
+
