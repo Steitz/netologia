d4eaed286cbf2f9fcf29e099f01afdf9a471c292
Steitz <shteizel@mail.ru>
2025-08-26 16:22:20 +0300

    Create TaxSystem.java

---
+++ Polymorphism/taxes/TaxSystem.java
@@ -0,0 +1,13 @@
+package taxes;
+
+public class TaxSystem {
+    public String typeTax = " ";
+
+    public int calcTaxFor(int debit, int credit) {
+        return 0;
+    }
+
+    public String typeTax() {
+        return typeTax;
+    }
+}
