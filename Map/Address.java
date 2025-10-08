1ed7d8f23f93182e1ac6953688dc46d24d62ae39
Steitz <shteizel@mail.ru>
2025-10-08 21:49:52 +0300

    Create Address.java

---
+++ Map/Address.java
@@ -0,0 +1,25 @@
+import java.util.Objects;
+
+public class Address {
+    protected String city;
+    protected String country;
+
+    Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Address o = (Address) obj;
+        return country.equals(o.country) && city.equals(o.city);
+    }
+
+
+}
