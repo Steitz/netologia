bdf331613dc83b4467e4056f4f3528554c6c062f
Steitz <shteizel@mail.ru>
2025-10-15 23:40:11 +0300

    Create Main.java

---
+++ Graphic/Main.java
@@ -0,0 +1,28 @@
+package ru.netology.graphics;
+import ru.netology.graphics.image.MyColorShema;
+import ru.netology.graphics.image.MyConverter;
+import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.server.GServer;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
+        try {
+            TextGraphicsConverter converter = new MyConverter(); // Создайте тут объект вашего класса конвертера
+
+            GServer server = new GServer(converter); // Создаём объект сервера
+            server.start(); // Запускаем
+
+            // Или то же, но с выводом на экран:
+            //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+            //String imgTxt = converter.convert(url);
+            //System.out.println(imgTxt);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
